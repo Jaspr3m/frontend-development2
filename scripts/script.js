062afc5a88975a25fe52beb2794387b19ad0679f
Jaspr3m <jasprem.singh@hva.nl>
2024-09-16 20:35:04 +0200

    BUTTONS WORK NOW YAAY

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 const buttons = document.querySelectorAll("button")
 
+function goToGames() {
+    window.location.href = "index.html"
+}
+
+function goToArt() {
+    window.location.href = "art.html"
+}
+
+buttons[1].addEventListener("click", goToGames)
+buttons[3].addEventListener("click", goToArt)
