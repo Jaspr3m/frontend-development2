8edcef8eded3f158e2741215b516949d51d95cd6
Jaspr3m <jasprem.singh@hva.nl>
2024-09-20 08:11:31 +0200

    changes

--- scripts/script.js
+++ scripts/script.js
@@ -8,5 +8,9 @@ function goToArt() {
     window.location.href = "art.html"
 }
 
+// als de pagina art.html is,   doe de tweede grid style en haal de eerste grid style weg
+// als de pagina index.html is, doe de eerste grid style en haal de tweede grid style weg
+
+
 buttons[1].addEventListener("click", goToGames)
 buttons[3].addEventListener("click", goToArt)
